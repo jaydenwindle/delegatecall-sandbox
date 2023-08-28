119a19c203ebdd5e9cdecd17768e0c2d8a5ad26a
Jayden Windle <jaydenwindle@gmail.com>
2023-08-27 21:27:03 -0400

    Added readme, MIT license spdx

---
+++ README.md
@@ -0,0 +1,3 @@
+# Delegatecall Sandbox
+
+A pattern for allowing arbitrary delegatecalls without the risk of storage collision attacks by using a dedicated sandbox contract.
--- src/lib/LibSandbox.sol
+++ src/lib/LibSandbox.sol
@@ -1,9 +1,10 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 library LibSandbox {
     bytes public constant header = hex"604380600d600039806000f3fe73";
-    bytes public constant footer = hex"3314601d573d3dfd5b363d3d373d3d6014360360143d5160601c5af43d6000803e80603e573d6000fd5b3d6000f3";
+    bytes public constant footer =
+        hex"3314601d573d3dfd5b363d3d373d3d6014360360143d5160601c5af43d6000803e80603e573d6000fd5b3d6000f3";
 
     function bytecode(address owner) external pure returns (bytes memory) {
         return abi.encodePacked(header, owner, footer);
